b8ba589d4bc63256d7986b1dee8d174feb552a7a
Uiri <uiri00@gmail.com>
2012-07-28 00:18:37 -0400

    Fuck you python and your damn parentheses

--- markdownwiki/models.py
+++ markdownwiki/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class Page(models.Model):
     title = models.CharField(max_length=255)
-    redirect = models.BooleanField
-    revision = models.PositiveIntegerField
-    contents = models.TextField
-    talkcontents = models.TextField
+    redirect = models.BooleanField()
+    revision = models.PositiveIntegerField()
+    contents = models.TextField()
+    talkcontents = models.TextField()
 
     def __unicode__(self):
         return self.contents
